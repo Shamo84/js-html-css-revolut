a478f50e90537bc3a643da56e62c7e75b9be0ffa
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-20 17:27:20 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
+  // alterna tra PERSONALE e BUSINESS
   $(".header-left li:not(:first-child) a").on("click", function() {
     $(".header-left a").removeClass("active");
     $(this).addClass("active");
   });
-  $(".header-right li").hover(function() {
+
+  // evidenzia di nero all'hover
+  $(".header-right li, .header-right li a").hover(function() {
     $(this).addClass("black");
   }, function() {
     $(this).removeClass("black");
   });
+
+  // cala il dropdown sull'hover
   $(".header-right li.dropdown-on-hover").hover(function() {
-    $(this).addClass("black");
+    $(this).children(".dropdown").show();
   }, function() {
-    $(this).removeClass("black");
+    $(this).children(".dropdown").hide();
+  });
+
+  // apre e chiude il dropdown sul click
+  $(".header-right li.dropdown-on-hover").on("click", function() {
+    $(this).children(".dropdown").toggle();
+  });
+
+  // alterna la freccina della lingua
+  $("#language").hover(function() {
+    $("#language .fa-angle-down").hide();
+    $("#language .fa-angle-up").show();
+  }, function() {
+    $("#language .fa-angle-up").hide();
+    $("#language .fa-angle-down").show();
   });
 });
