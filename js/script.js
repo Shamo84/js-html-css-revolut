8e3ce479ff42b0eb208760d9839d0f0d089f5068
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-20 16:31:28 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-
-
-
+  $(".header-left li:not(:first-child) a").on("click", function() {
+    $(".header-left a").removeClass("active");
+    $(this).addClass("active");
+  });
+  $(".header-right li").hover(function() {
+    $(this).addClass("black");
+  }, function() {
+    $(this).removeClass("black");
+  });
+  $(".header-right li.dropdown-on-hover").hover(function() {
+    $(this).addClass("black");
+  }, function() {
+    $(this).removeClass("black");
+  });
 });
