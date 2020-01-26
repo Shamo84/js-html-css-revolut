991ccdf36f81bb554d795a214beba97703a7cd49
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-26 15:22:15 +0100

    i click funzionano perfettamente

--- js/script.js
+++ js/script.js
@@ -15,13 +15,36 @@ $(document).ready(function() {
 
   // cala il dropdown sull'hover
   $(".header-right li.dropdown-on-hover").mouseenter(function() {
-    $(this).siblings().children(".dropdown").hide();
+    $(this).siblings().children(".dropdown").removeClass("display-block");
     $(this).siblings().children(".fas").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-    $(this).children(".dropdown").show();
+    $(this).children(".dropdown").addClass("display-block");
     $(this).children(".fas").removeClass("fa-chevron-down").addClass("fa-chevron-up");
   });
 
-  $(document).on("click", function() {
-      $(".dropdown").hide();
+  $(document).on("click", function(event) {
+    if ($(".dropdown").hasClass("display-block")) {
+      if ($(event.target).hasClass("dropdown-on-hover")) {
+        $(event.target).children(".dropdown").removeClass("display-block");
+        if ($(event.target).children("i").hasClass("fa-chevron-up")) {
+          $("#language").children(".fas").addClass("fa-chevron-down").removeClass("fa-chevron-up");
+        }
+      } else if ($(event.target).hasClass(".dropdown")) {
+      } else {
+        $(".dropdown.display-block").removeClass("display-block");
+        if ($(event.target).hasClass("fa-chevron-up")) {
+          $("#language").children(".fas").addClass("fa-chevron-down").removeClass("fa-chevron-up");
+        }
+      }
+    } else {
+      if ($(event.target).hasClass("dropdown-on-hover")) {
+        $(event.target).children(".dropdown").addClass("display-block");
+      } else if ($(event.target).hasClass("fa-chevron-down")) {
+        $("#language").children(".dropdown").addClass("display-block");
+        $("#language").children(".fas").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+      }
+      if ($(event.target).children("i").hasClass("fa-chevron-down")) {
+        $("#language").children(".fas").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+      }
+    }
   });
 });
